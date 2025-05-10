7f5b72d93100293ef7d1bc7bb194a0154506a42f
梁琦 <liangqi@msn.com>
2025-05-10 21:26:36 +0800

    setup fmea page

--- fema-web-client/src/app/pages/welcome/welcome.component.ts
+++ fema-web-client/src/app/pages/welcome/welcome.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-welcome',
+  imports: [NzLayoutModule, NzGridModule, NzStepsModule, NzCardModule, NzFlexModule, NzButtonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
-  constructor() {}
+  constructor() { }
 }
