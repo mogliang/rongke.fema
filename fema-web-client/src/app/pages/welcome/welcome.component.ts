8797abbea48fa33b06b93128a585d9cd2e538528
梁琦 <liangqi@msn.com>
2025-05-13 21:21:39 +0800

    a few updates

--- fema-web-client/src/app/pages/welcome/welcome.component.ts
+++ fema-web-client/src/app/pages/welcome/welcome.component.ts
@@ -8,12 +8,12 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { NzTreeModule } from 'ng-zorro-antd/tree';
-
-
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @Component({
   selector: 'app-welcome',
-  imports: [NzLayoutModule, NzGridModule, NzStepsModule, NzCardModule, NzFlexModule, NzButtonModule,NzTabsModule, NzRadioModule,NzTreeModule],
+  imports: [NzLayoutModule, NzGridModule, NzStepsModule, NzCardModule, NzFlexModule, NzButtonModule, NzTabsModule, NzRadioModule, NzTreeModule, NzTableModule, NzDividerModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
@@ -22,29 +22,36 @@ export class WelcomeComponent {
 
   readonly nodes = [
     {
-      title: 'FMEA编号/物料名',
+      icon: 'setting',
+      title: 'S0001/仓储',
       key: '00',
       expanded: true,
       children: [
         {
-          title: '结构ID/结构名',
+          icon: 'setting',
+          title: 'S0002/入库',
           key: '000',
           expanded: true,
           children: [
-            { title: '213123/轴', key: '0000', isLeaf: true },
-            { title: '213124/轴承', key: '0001', isLeaf: true },
-            { title: '213124/轴承', key: '0002', isLeaf: true }
+            { icon: 'setting', title: 'S0003/仓储员', key: '0000', isLeaf: true },
+            { icon: 'setting', title: 'S0003/物料管控系统', key: '0000', isLeaf: true },
           ]
         },
         {
-          title: '结构ID/结构名',
+          icon: 'setting',
+          title: 'S0004/存储',
           key: '001',
           children: [
-            { title: '213123/活塞筒', key: '0010', isLeaf: true },
-            { title: '213123/曲轴箱', key: '0011', isLeaf: true },
+            { icon: 'setting', title: 'S0005/环境', key: '0010', isLeaf: true },
+            { icon: 'setting', title: 'S0006/储位', key: '0011', isLeaf: true },
+            { icon: 'setting', title: 'S0006/堆叠高度', key: '0011', isLeaf: true },
+            { icon: 'setting', title: 'S0006/叉车', key: '0011', isLeaf: true },
+            { icon: 'setting', title: 'S0006/保质期', key: '0011', isLeaf: true },
+            { icon: 'setting', title: 'S0006/油漆仓温湿度管控', key: '0011', isLeaf: true },
           ]
         },
         {
+          icon: 'setting',
           title: '0-0-2',
           key: '002'
         }
@@ -52,4 +59,32 @@ export class WelcomeComponent {
     },
   ];
 
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: '1',
+      age: 32,
+      address: '轴'
+    },
+    {
+      key: '2',
+      name: '2',
+      age: 42,
+      address: '轴承'
+    },
+    {
+      key: '3',
+      name: '2',
+      age: 32,
+      address: '结构名'
+    }
+  ];
+
+}
+
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
 }
