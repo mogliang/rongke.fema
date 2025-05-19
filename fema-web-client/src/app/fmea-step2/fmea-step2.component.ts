5129b405a274e01b9149e23448d86ee8efdfe334
梁琦 <liangqi@msn.com>
2025-05-19 20:40:47 +0800

    add menu to tree

--- fema-web-client/src/app/fmea-step2/fmea-step2.component.ts
+++ fema-web-client/src/app/fmea-step2/fmea-step2.component.ts
@@ -4,21 +4,22 @@ import { NzTreeNodeOptions, NzTreeModule } from 'ng-zorro-antd/tree';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { FMStructureDto, FMStructuresService, TreeType } from '../../libs/api-client';
 import { Observable } from 'rxjs';
 import { NzSplitterModule } from 'ng-zorro-antd/splitter';
 import { HelperService } from '../helper.service';
-
+import { NzContextMenuService, NzDropdownMenuComponent, NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 @Component({
   selector: 'app-fmea-step2',
-  imports: [NzTabsModule, NzTreeModule, NzTableModule, NzDividerModule, NzCardModule, NzSplitterModule],
+  imports: [NzIconModule, NzDropDownModule, NzTabsModule, NzTreeModule, NzTableModule, NzDividerModule, NzCardModule, NzSplitterModule],
   providers: [FMStructuresService, HelperService],
   templateUrl: './fmea-step2.component.html',
   styleUrl: './fmea-step2.component.css'
 })
 export class FmeaStep2Component {
-  constructor(private fmStructureService: FMStructuresService, private helper: HelperService) { }
+  constructor(private nzContextMenuService: NzContextMenuService, private fmStructureService: FMStructuresService, private helper: HelperService) { }
 
   ngOnInit() {
     var fmStructure = this.fmStructureService.apiFMStructuresTreeCodeGet("S901002", TreeType.NUMBER_0);
@@ -27,6 +28,14 @@ export class FmeaStep2Component {
       this.nodes = node.children || [];
       this.fmStructures = this.helper.flattenFMStructures(data.childFMStructures || []);
     });
+  }
+
+  contextMenu($event: MouseEvent, menu: NzDropdownMenuComponent): void {
+    this.nzContextMenuService.create($event, menu);
+  }
+
+  selectDropdown(): void {
+    // do something
   }
 
   public fmStructures: FMStructureDto[] = [];
